857dc6df95e8c3357f4bb9df5c04113a9b184d34
cody gant <codygantol@gmail.com>
2022-03-23 15:20:07 -0400

    updating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
-
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import About from "./components/About";
 import Technologies from "./components/Tech";
 import ContactMe from "./components/Contactme";
 import Footer from "./components/footer";
 import { Stack } from "react-bootstrap";
 function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+    
+  }, []);
   return (
     <div className = 'App'>
       
--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,14 @@
 import React from "react";
-
+import {Container} from "react-bootstrap";
 function About() {
   return (
     <div className="aboutMe">
       <div className = 'aboutText'> 
-        <h1>Hello!</h1>
-        <h2>My Name is Cody Gant</h2>
-        <h3>
-          I'm a Front-End Developer and a Computer Software Engineering Student.
-        </h3>
+        <Container>
+            <h2>Hello!</h2>
+            <h3>My Name is Cody Gant.</h3>
+            <span>I'm a Front End Developer and a Software Enginering Student</span>
+        </Container>
       </div>
     </div>
   );
