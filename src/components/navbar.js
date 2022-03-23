51f230b098f7230d07775d3be7c5caab9075d875
cody gant <codygantol@gmail.com>
2022-03-23 13:34:15 -0400

    updating

---
+++ src/components/navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Navbar, Nav } from "react-bootstrap";
+
+
+export default function Header() {
+  return (
+    <div data-aos="fade-down" data-aos-easing="linear"
+    data-aos-duration="500" className = 'navbar'>
+<Navbar collapseOnSelect expand="lg" bg="transparent" variant="light" sticky='top'>
+    
+    <Navbar.Brand href="/" className = "test"></Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+        </Nav>
+        <Nav>
+       
+          <Nav.Link className = 'navLink' href="/Locations" >Locations</Nav.Link>
+          <Nav.Link className = 'navLink' href="/Menu">Menu</Nav.Link>
+          <Nav.Link className = 'navLink' href='/Reservations'>Reservations</Nav.Link>
+          <Nav.Link className = 'navLink' href="/privateDining">Private Dining</Nav.Link>
+          <Nav.Link className = 'navLink' href="/giftcards">Gift Cards</Nav.Link>
+          
+          <Nav.Link className = 'navLink' href="/butcher">Butcher Grille Box</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    
+  </Navbar>
+  </div>
+  )
+}
