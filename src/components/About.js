9d5de893e8a74d13d1312732e11f9a15ce0eaa92
cody gant <codygantol@gmail.com>
2022-03-24 12:12:17 -0400

    lets start!

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,85 @@
 import React from "react";
-import {Container} from "react-bootstrap";
+
 function About() {
   return (
-    <div className="aboutMe">
-      <div className = 'aboutText'> 
-        <Container>
-            <h2>Hello!</h2>
-            <h3>My Name is Cody Gant.</h3>
-            <span>I'm a Front End Developer and a Software Enginering Student</span>
-        </Container>
-      </div>
+    <div class="screen">
+      <section class="buttonGroup">
+        <h2>PORTFOLIO</h2>
+        <button value="Placebo Button">TECHNOLOGIES</button>
+        <button value="Rockets">PROJECTS</button>
+        <button value="Smoke Screen">RESUME</button>
+        <button value="Booster Rockets">HOW TO CONTACT ME</button>
+        <section class="setButtons">
+          <button value="Destroy"></button>
+          <button value="Deploy"></button>
+        </section>
+        <button value="Retrograde Torpedoes"></button>
+        <button value="Science Canon"></button>
+      </section>
+
+      <section class="buttonGroup">
+        <h2>ABOUT</h2>
+        <section class="imageDisplay">
+          <div class="aspect">
+          
+          </div>
+        </section>
+        <p>Hello...</p>
+        <p>
+          My name is Cody Gant. I'm a Front End Developer and Full Time Student at Daytona State College
+          Majoring in Software Enginering. My passion is in computers and their ability to form the world around us in a cyber framework.
+          <hr />
+          Sed rhoncus a leo congue sollicitudin. Fusce risus tortor, rutrum id
+          egestas at, elementum in dui. Fusce bibendum purus eu turpis
+          vestibulum, eget aliquet ante pellentesque. Class aptent taciti
+          sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos. Fusce egestas, mauris eu vehicula placerat, lacus mi
+          hendrerit lacus, ac rhoncus arcu purus eget odio. Aliquam commodo
+          mattis lorem vitae porttitor.
+        </p>
+      </section>
+
+      <section class="buttonGroup">
+        <h2>Scanning</h2>
+        <div class="visual1"></div>
+
+        <div class="visual2"></div>
+
+        <section class="setButtons">
+          <button value="1"></button>
+          <button value="2"></button>
+          <button value="3"></button>
+          <button value="4"></button>
+          <button value="x"></button>
+          <button value="x"></button>
+          <button value="x"></button>
+          <button value="x"></button>
+          <button value="x"></button>
+          <button value="x"></button>
+        </section>
+
+        <section>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit
+            amet est nisl. Mauris euismod vel quam eu tincidunt. Sed dapibus
+            justo id dapibus tempor. Pellentesque a libero sed risus congue
+            ultrices. Quisque at congue massa. Duis feugiat ligula non nisi
+            semper tempus. Duis blandit lacus sed diam blandit, id faucibus nunc
+            viverra. Morbi consequat consequat facilisis. Sed in felis vitae
+            nibh venenatis finibus sed et dolor. Sed vulputate molestie est ac
+            tristique. Vestibulum et aliquam velit, in convallis nisl. Vivamus
+            pellentesque venenatis urna eget suscipit.
+            <hr />
+            Sed rhoncus a leo congue sollicitudin. Fusce risus tortor, rutrum id
+            egestas at, elementum in dui. Fusce bibendum purus eu turpis
+            vestibulum, eget aliquet ante pellentesque. Class aptent taciti
+            sociosqu ad litora torquent per conubia nostra, per inceptos
+            himenaeos. Fusce egestas, mauris eu vehicula placerat, lacus mi
+            hendrerit lacus, ac rhoncus arcu purus eget odio. Aliquam commodo
+            mattis lorem vitae porttitor.
+          </p>
+        </section>
+      </section>
     </div>
   );
 }
